41896f8e3fad31c09ccab3e69009d61543ab4dbf
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-09-12 09:26:20 +0300

    Initial Commit

---
+++ AIImageGenerator/AIImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIImageGeneratorApp.swift
+//  AIImageGenerator
+//
+//  Created by Антон Разгуляев on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AIImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
