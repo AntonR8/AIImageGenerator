6bf61d03a9a15ecbde9670011a4cff672a906f02
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-10-03 10:34:22 +0300

    Убран ipad

--- AIImageGenerator/Views/Paywall/Elements/SubscriptionsOptionsView.swift
+++ AIImageGenerator/Views/Paywall/Elements/SubscriptionsOptionsView.swift
@@ -11,18 +11,25 @@ struct SubscriptionsOptionsView: View {
     @EnvironmentObject var vm: ViewModel
     
     var body: some View {
-        VStack {
-            Button(action: {
-                vm.chosenSubscription = vm.products[1]
-            }, label: {
-                SuscriptionButton(price: 19.99, bestOffer: false, subscription: vm.products[1])
-            })
-            .padding(.vertical, 8)
-            Button(action: {
-                vm.chosenSubscription = vm.products[0]
-            }, label: {
-                SuscriptionButton(price: 39.99, bestOffer: true, subscription: vm.products[0])
-            })
+        if vm.products.isEmpty {
+            Text("⚠️ no Internet connection")
+        } else {
+            VStack {
+                Button(action: {
+                    vm.chosenSubscription = vm.products[1]
+                }, label: {
+                    SuscriptionButton(
+                        price: 19.99,
+                        bestOffer: false,
+                        subscription: vm.products.isEmpty ? nil : vm.products[1])
+                })
+                .padding(.vertical, 8)
+                Button(action: {
+                    vm.chosenSubscription = vm.products[0]
+                }, label: {
+                    SuscriptionButton(price: 39.99, bestOffer: true, subscription: vm.products[0])
+                })
+            }
         }
     }
 }
--- AIImageGenerator/Views/Paywall/Elements/SuscriptionButton.swift
+++ AIImageGenerator/Views/Paywall/Elements/SuscriptionButton.swift
@@ -13,47 +13,49 @@ struct SuscriptionButton: View {
     @EnvironmentObject var vm: ViewModel
     var price: Double
     let bestOffer: Bool
-    let subscription: Product
+    let subscription: Product?
 
-    var body: some View { 
-        HStack() {
-            Image(systemName: vm.chosenSubscription == subscription ? "checkmark.circle.fill" : "circle")
-            VStack(alignment: .leading) {
-                Group {
-                    Text(subscription == vm.products[1] ? "Monthly" : "Yearly") + Text(" · $").foregroundStyle(.secondary) + Text(price.description).foregroundStyle(.secondary)
+    var body: some View {
+
+            HStack() {
+                Image(systemName: vm.chosenSubscription == subscription ? "checkmark.circle.fill" : "circle")
+                VStack(alignment: .leading) {
+                    Group {
+                        Text(subscription == vm.products[1] ? "Monthly" : "Yearly") + Text(" · $").foregroundStyle(.secondary) + Text(price.description).foregroundStyle(.secondary)
+                    }
+                    .font(.caption)
+                    Text("$") + Text((String(format: "%.2f",
+                                             subscription == vm.products[1]  ? price/4-0.01 : price/21.385))) + Text(" / week")
                 }
-                .font(.caption)
-                Text("$") + Text((String(format: "%.2f",
-                                         subscription == vm.products[1]  ? price/4-0.01 : price/21.385))) + Text(" / week")
-            }
-            .foregroundStyle(.white)
-            if bestOffer {
-                BestOfferBadgeView()
-            }
+                .foregroundStyle(.white)
+                if bestOffer {
+                    BestOfferBadgeView()
+                }
 
-            Spacer()
-            Divider()
-                .frame(height: 30)
-            VStack {
-                Text("3 days")
-                Text("free")
+                Spacer()
+                Divider()
+                    .frame(height: 30)
+                VStack {
+                    Text("3 days")
+                    Text("free")
+                }
+                .foregroundStyle(.accent)
+                .font(.caption)
             }
-            .foregroundStyle(.accent)
-            .font(.caption)
-        }
-        .padding(8)
-        .padding(.horizontal, 8)
-        .background(.gray.opacity(0.3))
-        .clipShape(Capsule())
-        .overlay {
-            Capsule().stroke(vm.chosenSubscription == subscription ? .accent : .clear)
+            .padding(8)
+            .padding(.horizontal, 8)
+            .background(.gray.opacity(0.3))
+            .clipShape(Capsule())
+            .overlay {
+                Capsule().stroke(vm.chosenSubscription == subscription ? .accent : .clear)
+            
         }
     }
 }
 
-//#Preview {
-//    SuscriptionButton(price: 19.99, bestOffer: true, subscription: .monthly)
-//        .environmentObject(ViewModel())
-//}
+#Preview {
+    SuscriptionButton(price: 19.99, bestOffer: true, subscription: nil)
+        .environmentObject(ViewModel())
+}
 
 
